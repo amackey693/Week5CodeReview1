d752d2e5860ff964c6193803cbcadf26a3282866
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 12:00:20 -0700

    fail initial test on spec 'method planetCalc() adds properties to constructor if method is run, otherwise they are not part of the constructor'

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -5,60 +5,12 @@ import { Age } from "./../src/calculator"
 
 // TESTS FOR AGE CALCULATOR 
 describe ("Age", () => {
-  test ('Age constructor holds values for UserInput and mercuryAge is set to zero', () => {
+  test ('Age constructor holds values for UserInput', () => {
     let newAge = new Age (26);
     expect(newAge.userInput).toEqual(26);
-    expect(newAge.mercuryAge).toEqual(0);
+  
   }); 
 
-  // test ('Age method mercuryCalc() returns proper age in mercury years based off of the users inputted age', () => {
-  //   let newAge = new Age(26);
-  //   newAge.mercuryCalc();
-  //   expect(newAge.mercuryAge).toBeGreaterThanOrEqual(107.840909)
-  // });
-  test ('Age constructor holds values for UserInput and confirms mercuryAge/venusAge are both set to zero', () => {
-    let newAge = new Age (26);
-    expect(newAge.userInput).toEqual(26);
-    expect(newAge.mercuryAge).toEqual(0);
-    expect(newAge.venusAge).toEqual(0);
-  }); 
-  // test ('Age method venusCalc() returns proper age in venus years based off of the users inputted age', () => {
-  //   let newAge = new Age(26);
-  //   newAge.venusCalc();
-  //   expect(newAge.venusAge).toBeGreaterThanOrEqual(42);
-  // });
-  test ('Age constructor holds values for UserInput and confirms new property marsAge is set to zero', () => {
-    let newAge = new Age (26);
-    expect(newAge.userInput).toEqual(26);
-    expect(newAge.marsAge).toEqual(0);
-  }); 
-  // test ('Age method marsCalc() returns proper age in mars years based off of the users inputted age', () => {
-  //   let newAge = new Age(26);
-  //   newAge.marsCalc();
-  //   expect(newAge.marsAge).toBeGreaterThanOrEqual(13);
-  // });
-  test ('Age constructor holds values for UserInput and confirms new property jupiterAge is set to zero', () => {
-    let newAge = new Age (26);
-    expect(newAge.userInput).toEqual(26);
-    expect(newAge.jupiterAge).toEqual(0);
-  }); 
-  // test ('Age method jupiterCalc() returns proper age in jupiter years based off of the users inputted age', () => {
-  //   let newAge = new Age(26);
-  //   newAge.jupiterCalc();
-  //   expect(newAge.jupiterAge).toBeGreaterThanOrEqual(2);
-  // });
-  // test ('All age methods will return the age rounded to the nearest whole number ', () => {
-  //   let newAge = new Age(26);
-  //   newAge.mercuryCalc();
-  //   newAge.venusCalc();
-  //   newAge.marsCalc();
-  //   newAge.jupiterCalc();
-
-  //   expect(newAge.mercuryAge).toEqual(108);
-  //   expect(newAge.venusAge).toEqual(42);
-  //   expect(newAge.marsAge).toEqual(14);
-  //   expect(newAge.jupiterAge).toEqual(2);
-  // });
   test ('Age method planetCalc() compiles all other methods to create one large method based off of the user selection', () => {
     let newAge = new Age(26);
     newAge.planetCalc("Mercury");
@@ -70,6 +22,19 @@ describe ("Age", () => {
     expect(newAge.venusAge).toEqual(42);
     expect(newAge.marsAge).toEqual(14);
     expect(newAge.jupiterAge).toEqual(2);
-  })
+  });
+
+  test ('Age method planetCalc() adds all properties to constructor if they are run, otherwise they are not a part of the constructor', () => {
+    let newAge = new Age(26);
+    newAge.planetCalc("Mercury");
+    newAge.planetCalc("Venus");
+    newAge.planetCalc("Mars");
+    newAge.planetCalc("Jupiter");
+
+    expect(newAge.mercuryAge).toEqual(null);
+    expect(newAge.venusAge).toEqual(null);
+    expect(newAge.marsAge).toEqual(null);
+    expect(newAge.jupiterAge).toEqual(null);
+  });
 })
 
