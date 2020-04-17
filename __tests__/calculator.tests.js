b32bed256fbc27f765262a32fea783bf2f88e6f7
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 11:52:39 -0700

    add logic to pass test on spec 'planetCalc() method compiles all other methods to create one large method based off of the user selection'

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -11,42 +11,42 @@ describe ("Age", () => {
     expect(newAge.mercuryAge).toEqual(0);
   }); 
 
-  test ('Age method mercuryCalc() returns proper age in mercury years based off of the users inputted age', () => {
-    let newAge = new Age(26);
-    newAge.mercuryCalc();
-    expect(newAge.mercuryAge).toBeGreaterThanOrEqual(107.840909)
-  });
+  // test ('Age method mercuryCalc() returns proper age in mercury years based off of the users inputted age', () => {
+  //   let newAge = new Age(26);
+  //   newAge.mercuryCalc();
+  //   expect(newAge.mercuryAge).toBeGreaterThanOrEqual(107.840909)
+  // });
   test ('Age constructor holds values for UserInput and confirms mercuryAge/venusAge are both set to zero', () => {
     let newAge = new Age (26);
     expect(newAge.userInput).toEqual(26);
     expect(newAge.mercuryAge).toEqual(0);
     expect(newAge.venusAge).toEqual(0);
   }); 
-  test ('Age method venusCalc() returns proper age in venus years based off of the users inputted age', () => {
-    let newAge = new Age(26);
-    newAge.venusCalc();
-    expect(newAge.venusAge).toBeGreaterThanOrEqual(42);
-  });
+  // test ('Age method venusCalc() returns proper age in venus years based off of the users inputted age', () => {
+  //   let newAge = new Age(26);
+  //   newAge.venusCalc();
+  //   expect(newAge.venusAge).toBeGreaterThanOrEqual(42);
+  // });
   test ('Age constructor holds values for UserInput and confirms new property marsAge is set to zero', () => {
     let newAge = new Age (26);
     expect(newAge.userInput).toEqual(26);
     expect(newAge.marsAge).toEqual(0);
   }); 
-  test ('Age method marsCalc() returns proper age in mars years based off of the users inputted age', () => {
-    let newAge = new Age(26);
-    newAge.marsCalc();
-    expect(newAge.marsAge).toBeGreaterThanOrEqual(13);
-  });
+  // test ('Age method marsCalc() returns proper age in mars years based off of the users inputted age', () => {
+  //   let newAge = new Age(26);
+  //   newAge.marsCalc();
+  //   expect(newAge.marsAge).toBeGreaterThanOrEqual(13);
+  // });
   test ('Age constructor holds values for UserInput and confirms new property jupiterAge is set to zero', () => {
     let newAge = new Age (26);
     expect(newAge.userInput).toEqual(26);
     expect(newAge.jupiterAge).toEqual(0);
   }); 
-  test ('Age method jupiterCalc() returns proper age in jupiter years based off of the users inputted age', () => {
-    let newAge = new Age(26);
-    newAge.jupiterCalc();
-    expect(newAge.jupiterAge).toBeGreaterThanOrEqual(2);
-  });
+  // test ('Age method jupiterCalc() returns proper age in jupiter years based off of the users inputted age', () => {
+  //   let newAge = new Age(26);
+  //   newAge.jupiterCalc();
+  //   expect(newAge.jupiterAge).toBeGreaterThanOrEqual(2);
+  // });
   test ('All age methods will return the age rounded to the nearest whole number ', () => {
     let newAge = new Age(26);
     newAge.mercuryCalc();
@@ -61,13 +61,15 @@ describe ("Age", () => {
   });
   test ('Age method planetCalc() compiles all other methods to create one large method based off of the user selection', () => {
     let newAge = new Age(26);
-    newAge.planetCalc("choice");
-    expect(newAge.mercuryAge).toBeGreaterThanOrEqual(null);
-    expect(newAge.venusAge).toBeGreaterThanOrEqual(null);
-    expect(newAge.marsAge).toBeGreaterThanOrEqual(null);
-    expect(newAge.jupiterAge).toBeGreaterThanOrEqual(null);
+    newAge.planetCalc("Mercury");
+    newAge.planetCalc("Venus");
+    newAge.planetCalc("Mars");
+    newAge.planetCalc("Jupiter");
+
+    expect(newAge.mercuryAge).toEqual(108);
+    expect(newAge.venusAge).toEqual(42);
+    expect(newAge.marsAge).toEqual(14);
+    expect(newAge.jupiterAge).toEqual(2);
   })
-
-
 })
 
--- src/calculator.js
+++ src/calculator.js
@@ -8,32 +8,48 @@ export class Age {
     this.jupiterAge = 0;
   }
   //create one big "planetCalc()" method?? revisit at the end? 
-  // planetCalc() {
-  //   let num = this.userInput * 365;
+  planetCalc(choice) {
+    let num = this.userInput * 365;
     
+    if (choice === "Mercury") {
+      let newAge = Math.round(num/88); //number of days it takes Mercury to orbit the sun
+      this.mercuryAge = this.mercuryAge + newAge;
+    } 
+    if (choice === "Venus") {
+      let newAge = Math.round(num/225); //number of days it takes Venus to orbit the sun
+      this.venusAge = this.venusAge + newAge;
+    }
+    if (choice === "Mars") {
+      let newAge = Math.round(num/687); //number of days it takes Mars to orbit the sun
+      this.marsAge = this.marsAge + newAge;
+    }
+    if (choice === "Jupiter") {
+      let newAge = Math.round(num/(11.8 * 365)); //number of days it takes Jupiter to orbit the sun
+      this.jupiterAge = this.jupiterAge + newAge;
+    }
+  }
+
+  // mercuryCalc() {
+  //   let num = this.userInput * 365; //number of days in a year
+  //   let newAge = Math.round(num/88); //number of days it takes Mercury to orbit the sun
+  //   this.mercuryAge = this.mercuryAge + newAge;
+  // } 
+
+  // venusCalc() {
+  //   let num = this.userInput * 365; 
+  //   let newAge = Math.round(num/225); //number of days it takes Venus to orbit the sun
+  //   this.venusAge = this.venusAge + newAge;
   // }
 
-  mercuryCalc() {
-    let num = this.userInput * 365; //number of days in a year
-    let newAge = Math.round(num/88); //number of days it takes Mercury to orbit the sun
-    this.mercuryAge = this.mercuryAge + newAge;
-  } 
+  // marsCalc() {
+  //   let num = this.userInput * 365; 
+  //   let newAge = Math.round(num/687); //number of days it takes Mars to orbit the sun
+  //   this.marsAge = this.marsAge + newAge;
+  // }
 
-  venusCalc() {
-    let num = this.userInput * 365; 
-    let newAge = Math.round(num/225); //number of days it takes Venus to orbit the sun
-    this.venusAge = this.venusAge + newAge;
-  }
-
-  marsCalc() {
-    let num = this.userInput * 365; 
-    let newAge = Math.round(num/687); //number of days it takes Mars to orbit the sun
-    this.marsAge = this.marsAge + newAge;
-  }
-
-  jupiterCalc() {
-    let num = this.userInput * 365; 
-    let newAge = Math.round(num/(11.8 * 365)); //number of days it takes Jupiter to orbit the sun
-    this.jupiterAge = this.jupiterAge + newAge;
-  }
-}
+//   jupiterCalc() {
+//     let num = this.userInput * 365; 
+//     let newAge = Math.round(num/(11.8 * 365)); //number of days it takes Jupiter to orbit the sun
+//     this.jupiterAge = this.jupiterAge + newAge;
+//   }
+// }
