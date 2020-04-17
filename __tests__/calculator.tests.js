7ee67cfb17c59bedbad1267f588712ae72b3ce18
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 10:19:03 -0700

    test on mercuryCalc() method passes

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -14,7 +14,7 @@ describe ("Mercury", () => {
   test ('Mercury method mercuryCalc() returns proper age in mercury years based off of the users inputted age', () => {
     let newAge = new Mercury(26);
     newAge.mercuryCalc();
-    expect(newAge.mercuryAge).toEqual(107.840909)
+    expect(newAge.mercuryAge).toBeGreaterThanOrEqual(107.840909)
   });
 })
 
--- src/calculator.js
+++ src/calculator.js
@@ -5,11 +5,10 @@ export class Mercury {
     this.mercuryAge = 0
   }
 
-  mercuryCalc(age) {
-    let age = this.UserInput;
-    num = age * 365; //number of days in a year
+  mercuryCalc() {
+    let num = this.userInput * 365; //number of days in a year
     let newAge = num/88; //number of days it takes Mercury to orbit the sun
-    this.mercuryAge = newAge;
+    this.mercuryAge = this.mercuryAge + newAge;
   } 
 
 }
