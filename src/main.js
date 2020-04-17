2bff5d11b2e8187b12e4abaf9a9ef59f4e07b702
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 14:04:59 -0700

    add initial UI to main.js

--- src/main.js
+++ src/main.js
@@ -9,9 +9,30 @@ $(document).ready(function(){
   $("#form").submit(function(event){
     event.preventDefault();
     const userInput = parseInt($("#user-input").val());
-    const lifeTerm = parseInt($("#life-expectancy").val());
+    const lifeTerm = parseInt($("#life-term").val());
+    let userSelection = $(".user-selection").val();
 
     let newAge = new Age(userInput, lifeTerm);
-    newAge.planetCalc()
+    newAge.planetCalc();
+
+    if ((userInput === 0 || isNaN(userInput)) || (lifeTerm === 0 || isNaN(lifeTerm)) || (userSelection === " ")) {
+      $("#output").html("<p>Please Complete the form above to get your answer! <p>")
+    } 
+    else if (userSelection === "mercury") {
+      $("#output").html("<p> Your current age on Mercury is: " + newAge.all[0] + "</p>");
+      $("#output").html("<p> You've surpassed your life expectancy by: " + Math.abs(yearsLeft[0]) + " years!</p>");
+    }
+    else if (userSelection === "venus") {
+      $("#output").html("<p> Your current age on Venus is: " + newAge.all[1] + " years old</p>");
+      $("#output").html("<p> You still have " + yearsLeft[1] + " on Venus! </p>");
+    }
+    else if (userSelection === "mars") {
+      $("#output").html("<p> Your current age on Mars is: " + newAge.all[2] + " years old</p>");
+      $("#output").html("<p> You still have " + yearsLeft[2] + " years left to on Mars! </p>");
+    }
+    else if (userSelection === "jupiter") {
+      $("#output").html("<p> Your current age on Jupiter is: " + newAge.all[3] + " years old</p>");
+      $("#output").html("<p> You still have " + yearsLeft[3] + " years left to on Mars! </p>");
+    }
   });
 });
