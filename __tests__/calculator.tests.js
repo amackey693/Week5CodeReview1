1eadefb6c1d0d779ffb08c711003c3ec0aacc6da
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 13:40:55 -0700

    reconfigure all tests to be specific and current. comment out all previous methods to only use one if statementa

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -5,42 +5,17 @@ import { Age } from "./../src/calculator"
 
 // TESTS FOR AGE CALCULATOR 
 describe ("Age", () => {
-  test ('Age constructor holds values for UserInput', () => {
-    let newAge = new Age (26);
+  test ('Age constructor holds values for userInput & lifeTerm', () => {
+    let newAge = new Age (26, 86);
     expect(newAge.userInput).toEqual(26);
-  
+    expect(newAge.lifeTerm).toEqual(86);
   }); 
-
-  test ('Age method planetCalc() compiles all other methods to create one large method based off of the user selection', () => {
-    let newAge = new Age(26);
-    newAge.planetCalc("Mercury");
-    newAge.planetCalc("Venus");
-    newAge.planetCalc("Mars");
-    newAge.planetCalc("Jupiter");
-
-    expect(newAge.mercuryAge).toEqual(108);
-    expect(newAge.venusAge).toEqual(42);
-    expect(newAge.marsAge).toEqual(14);
-    expect(newAge.jupiterAge).toEqual(2);
-  });
-
-  test ('Age method planetCalc() adds all properties to constructor if they are run, otherwise they are not a part of the constructor', () => {
-    let newAge = new Age(26);
-    newAge.planetCalc("Mercury");
-    newAge.planetCalc("Venus");
-    newAge.planetCalc("Mars");
-    newAge.planetCalc("Jupiter");
-    expect(newAge.mercuryAge).toEqual(108);
-    expect(newAge.venusAge).toEqual(42);
-    expect(newAge.marsAge).toEqual(14);
-    expect(newAge.jupiterAge).toEqual(2);
-  });
   test ('Add option for "All" to age method planetCalc() that runs and displays all new ages to the user ', () => {
     let newAge = new Age(26);
     newAge.planetCalc("All");
     expect(newAge.all).toEqual([108, 42, 14, 2]);
   });
-  test ('Add property for lifeTerm to Age class and reconfigure planetCalc() to add property yearsLeft & determine how many years the user has to live on each planet', () => {
+  test ('Reconfigure planetCalc() method to add property yearsLeft & determine how many years the user has to live on each planet', () => {
     let newAge = new Age(26, 86);
     newAge.planetCalc("All");
     expect(newAge.yearsLeft).toEqual([-22, 44, 72, 84]);
--- src/calculator.js
+++ src/calculator.js
@@ -18,18 +18,20 @@ export class Age {
       this.all = [mercury, venus, mars, jupiter];
       this.yearsLeft = [num1-mercury, num1-venus, num1-mars, num1-jupiter];
     }
-    else if (choice === "Mercury") {
-      this.mercuryAge = Math.round(num/88);
-    } 
-    else if (choice === "Venus") {
-      this.venusAge = Math.round(num/225);
-    }
-    else if (choice === "Mars") {
-      this.marsAge = Math.round(num/687); 
-    }
-    else if (choice === "Jupiter") {
-      this.jupiterAge =  Math.round(num/(11.8 * 365)); 
-    }
   }
 }
-   // this.all = ["Mercury Age: " + mercury + ",", "Venus Age: " + venus + ",", "Mars Age: " + mars + ",", "Jupiter Age: "+ jupiter +"."]
+  //   }
+  //   else if (choice === "Mercury") {
+  //     this.mercuryAge = Math.round(num/88);
+  //   } 
+  //   else if (choice === "Venus") {
+  //     this.venusAge = Math.round(num/225);
+  //   }
+  //   else if (choice === "Mars") {
+  //     this.marsAge = Math.round(num/687); 
+  //   }
+  //   else if (choice === "Jupiter") {
+  //     this.jupiterAge =  Math.round(num/(11.8 * 365)); 
+  //   }
+  // }
+  // this.all = ["Mercury Age: " + mercury + ",", "Venus Age: " + venus + ",", "Mars Age: " + mars + ",", "Jupiter Age: "+ jupiter +"."]
