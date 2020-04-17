6b9d4473a1473b4dabbeb8ba626ad1f6f75efe3e
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 13:47:57 -0700

    reconfigure test/code. run all tests again

--- __tests__/calculator.tests.js
+++ __tests__/calculator.tests.js
@@ -10,14 +10,14 @@ describe ("Age", () => {
     expect(newAge.userInput).toEqual(26);
     expect(newAge.lifeTerm).toEqual(86);
   }); 
-  test ('Add option for "All" to age method planetCalc() that runs and displays all new ages to the user ', () => {
+  test ('Add method planetCalc() that runs and displays all new ages to the user ', () => {
     let newAge = new Age(26);
-    newAge.planetCalc("All");
+    newAge.planetCalc();
     expect(newAge.all).toEqual([108, 42, 14, 2]);
   });
   test ('Reconfigure planetCalc() method to add property yearsLeft & determine how many years the user has to live on each planet', () => {
     let newAge = new Age(26, 86);
-    newAge.planetCalc("All");
+    newAge.planetCalc();
     expect(newAge.yearsLeft).toEqual([-22, 44, 72, 84]);
   });
 })
