d0fcbe5e0dbd27e5de81b66254a29b555b38af37
Allison Mackey <a.mackey693@gmail.com>
2020-04-17 10:59:02 -0700

    add logic to pass test

--- src/calculator.js
+++ src/calculator.js
@@ -4,6 +4,7 @@ export class Age {
     this.userInput = userInput;
     this.mercuryAge = 0;
     this.venusAge = 0; 
+    this.marsAage = 0;
   }
   //create one big "planetCalc()" method?? revisit at the end? 
   mercuryCalc() {
